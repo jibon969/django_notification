ab597a5562ba8bc0da1895041f0fe63370e53ac7
jayed969 <jayed.swe@gmail.com>
2023-06-17 16:54:02 +0600

    added notification

--- dashboard/notifications_context_processor.py
+++ dashboard/notifications_context_processor.py
@@ -2,12 +2,14 @@ from .models import Notification
 
 
 def notification_value(request):
-    unread_notifications = Notification.objects.filter(user=request.user, read=False).count()
-    four_unread_notifications = Notification.objects.filter(
-        user=request.user, read=False).order_by('-created_at')[:4]
-    context = {
-        "unread_notifications": unread_notifications,
-        "four_unread_notifications": four_unread_notifications
-
-    }
-    return context
+    try:
+        unread_notifications = Notification.objects.filter(user=request.user, read=False).count()
+        four_unread_notifications = Notification.objects.filter(
+            user=request.user, read=False).order_by('-created_at')[:4]
+        context = {
+            "unread_notifications": unread_notifications,
+            "four_unread_notifications": four_unread_notifications
+        }
+        return context
+    except:
+        pass
